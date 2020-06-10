8932a9e37739471969c8db53b7acae20af954eb0
honshin4n1 <million_films.sh@icloud.com>
2020-06-10 19:35:48 +0900

    add-show-action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
   def index
-    
   end
 
   def show
+    @genre = Genre.new
   end
 
+  def create
+    @genre = Genre.new(genre_params)
+    
+  end
+
+
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name).merge(user_id: current_user.id)
+  end
 end
