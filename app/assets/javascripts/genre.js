ad08abf0b703bb45adeacb524b21b553771227fa
honshin4n1 <million_films.sh@icloud.com>
2020-07-16 11:35:34 +0900

    add search.js

--- app/assets/javascripts/genre.js
+++ app/assets/javascripts/genre.js
@@ -1,11 +1,9 @@
-
 window.addEventListener('DOMContentLoaded', function() {
     const speech = new webkitSpeechRecognition();
         speech.lang = 'ja-JP';
         // document.getElementById や addEventListener
         const StartGenre = document.getElementById('genre-btn');
         const ResultGenre = document.getElementById('genre__name');
-
         speech.addEventListener('result', function (e) {
             var text = '';
         
@@ -14,59 +12,16 @@ window.addEventListener('DOMContentLoaded', function() {
             }
             ResultGenre.value = text;
         }, false);
-
         StartGenre.addEventListener('click', function () {
             ResultGenre.value = '';
             speech.start();
         }, false);
-        
 },false);
 
-window.addEventListener('DOMContentLoaded', function() {
-    const speech = new webkitSpeechRecognition();
-        speech.lang = 'ja-JP';
-        // document.getElementById や addEventListener
-        const StartTitle = document.getElementById('title-btn');
-        const ResultTitle = document.getElementById('contents__title');
+
+
+ 
   
-        speech.addEventListener('result', function (e) {
-            var text = '';
-        
-            for (var i = 0; i < e.results.length; i++) {
-                text += e.results[i][0].transcript;
-            }
-            ResultTitle.value = text;
-        }, false);
-  
-        StartTitle.addEventListener('click', function () {
-            ResultTitle.value = '';
-            speech.start();
-        }, false);
-  
-  },false);
-  
-  
-  window.addEventListener('DOMContentLoaded', function() {
-    const speech = new webkitSpeechRecognition();
-        speech.lang = 'ja-JP';
-        // document.getElementById や addEventListener
-        const StartComment = document.getElementById('comment-btn');
-        const ResultComment = document.getElementById('contents__comment');
-  
-        speech.addEventListener('result', function (e) {
-          var text = '';
-      
-          for (var i = 0; i < e.results.length; i++) {
-              text += e.results[i][0].transcript;
-          }
-          ResultComment.value = text;
-      }, false);
-  
-      StartComment.addEventListener('click', function () {
-          ResultComment.value = '';
-          speech.start();
-      }, false);
-  
-        
-  },false);
+
+
   
