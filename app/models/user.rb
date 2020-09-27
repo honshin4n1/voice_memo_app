edcfada8208b743e2df977bdb39ab79afdec7789
honshin4n1 <million_films.sh@icloud.com>
2020-09-27 13:26:00 +0900

    create-validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 8..128
-         validates :name, presence: true
+         validates :name, presence: true, uniqueness: true
          validates :email, presence: true, uniqueness: true, format: { with: /\A[\x21-\x3f\x41-\x7e]+@(?:[-a-z0-9]+\.)+[a-z]{2,}\z/i }
   has_many :genres, dependent: :destroy
   has_many :contents, dependent: :destroy
