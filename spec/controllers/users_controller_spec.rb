6c9f16b5dcb38769a549f06ccf3cbdab9edefe1d
honshin4n1 <million_films.sh@icloud.com>
2020-11-17 18:50:57 +0900

    Update users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 describe UsersController do
-
+  let(:user) { create(:user)}
+  let(:content) { create(:content)}
+  describe 'GET #show' do
+    context 'ログインしている場合' do
       before do
+        login user
+        get :show, params: {id: user.id}
+      end
+      it "show.html.hamlに遷移すること" do
+        expect(response).to render_template :show
+      end
+      
+    #   it '@contentに期待した値が入っていること' do
+    #     contents = current_user.contents
+    #     expect(assigns(:contents)).to match(contents)
+    #   end
+    # end
+
+    context 'ログインしていない場合' do
+      before do
+        get :show, params: {id: user.id}
+      end
+      it 'top画面にリダイレクトすること' do
+      expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 end
