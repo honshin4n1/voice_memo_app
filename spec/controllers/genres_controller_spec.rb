45d9a928857e7a6865b1e6957e82ec23cc5f25a8
honshin4n1 <million_films.sh@icloud.com>
2020-12-02 17:00:38 +0900

    genre保存に成功した場合のテスト記述

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe GenresController do
-  let(:user) { create(:user)}
+  let(:user) { create(:user) }
+  let(:genre) { create(:genre) }
   describe '#index' do
     context 'ログインしている場合' do
       before do
         login user
-        get :index, params: {user_id: user.id}
+        get :index, params: {user_id: user.id, genre_id: genre.id}
       end
       it "index.html.hamlに遷移すること" do
         expect(response).to render_template :index
@@ -15,20 +16,33 @@ describe GenresController do
         expect(assigns(:genre)).to be_a_new(Genre)
       end
     end
+    context 'ログインしていない場合' do
+      before do
+        get :index, params: {genre_id: genre.id}
+      end
+      it 'index.html.hamlに遷移すること' do
+        expect(response).to render_template :index
+      end
+    end
   end
-  # describe '#create' do
-  #   let(:params) { {genre_id: genre.id, user_id: user.id}}
-  #   context 'ログインしている場合' do
-  #     before do
-  #       login user
-  #     end
-  #     context '保存に成功した場合'do
-  #       subject {post :create, params: params}
+  describe '#create' do
+    let(:params) { {user_id: user.id, genre: attributes_for(:genre)}}
+    context 'ログインしている場合' do
+      before do
+        login user
+      end
+      context '保存に成功した場合'do
+        subject {post :create, params: params}
 
-  #       it 'genreを保存すること' do
-  #         expect{ subject }.to change(Genre, :count).by(1)
-  #       end
-  #     end
-  #   end
-  # end
+        it 'genreを保存すること' do
+          expect{ subject }.to change(Genre, :count).by(1)
+        end
+        it 'root_pathへリダイレクトすること' do
+          subject
+          expect(response).to redirect_to(root_path)
+        end
+      end
+      context '保存に失敗した場合'
+    end
+  end
 end
