20898e65bc9dfbd87d50b9c257887c0dd08c6010
honshin4n1 <million_films.sh@icloud.com>
2020-12-03 11:24:24 +0900

    genres_controller test #index

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -6,14 +6,19 @@ describe GenresController do
   describe '#index' do
     context 'ログインしている場合' do
       before do
-        login user
-        get :index, params: {user_id: user.id, genre_id: genre.id}
+        @user = FactoryBot.create(:user) 
+        login @user
+        get :index, params: {user_id: @user.id, genre_id: genre.id}
       end
       it "index.html.hamlに遷移すること" do
         expect(response).to render_template :index
       end
       it "@genreに期待した値が入っていること" do
         expect(assigns(:genre)).to be_a_new(Genre)
+      end
+      it "@genresに期待した値が入っていること" do
+        genres = create_list(:genre, 3, user_id: @user.id)
+        expect(assigns(:genres)).to match(genres)
       end
     end
     context 'ログインしていない場合' do
@@ -42,7 +47,31 @@ describe GenresController do
           expect(response).to redirect_to(root_path)
         end
       end
-      context '保存に失敗した場合'
+      context '保存に失敗した場合' do
+        let(:invalid_params) {{user_id: user.id, genre: attributes_for(:genre, name: nil)}}
+        subject {post :create, params: invalid_params}
+        it 'genreを保存しないこと' do
+          expect{ subject }.not_to change(Genre, :count)
+        end
+        it 'index.html.haml へ遷移すること' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+  end
+  describe 'GET #edit' do
+    context 'ログインしている場合' do
+      before do
+        login user
+        get :edit, params: {user_id: user.id, genre_id: genre.id}
+      end
+      it "edit.html.hamlに遷移すること" do
+        expect(response).to render_template :edit
+      end
+      it "@genreに期待した値が入っていること" do
+        expect(assigns(:genre)).to be_a_new(Genre)
+      end
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 describe UsersController do
-  let(:user) { create(:user)}
-  let(:content) { create(:content)}
   describe 'GET #show' do
     context 'ログインしている場合' do
       before do
-        login user
-        get :show, params: {id: user.id}
+        @user = FactoryBot.create(:user) 
+        login @user
+        get :show, params: {id: @user.id}
       end
       it "show.html.hamlに遷移すること" do
         expect(response).to render_template :show
       end
       
-      # it '@contentに期待した値が入っていること' do
-      #   contents = current_user.contents
-      #   expect(assigns(:contents)).to match(contents)
-      # end
+      it '@contentsに期待した値が入っていること' do
+        contents = create_list(:content, 3, user_id: @user.id)
+        expect(assigns(:contents)).to match(contents)
+      end
     end
 
     context 'ログインしていない場合' do
       before do
-        get :show, params: {id: user.id}
+        @user = FactoryBot.create(:user) 
+        get :show, params: {id: @user.id}
       end
       it 'top画面にリダイレクトすること' do
       expect(response).to redirect_to(root_path)
