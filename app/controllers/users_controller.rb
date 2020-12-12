f994ca82ecdaed4f13625dbb2f6a4b6d334922ab
honshin4n1 <million_films.sh@icloud.com>
2020-12-12 21:56:57 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,7 @@ class UsersController < ApplicationController
     @genre = Genre.new
     @genres = current_user&.genres
     @content = Content.new
-    user = User.find_by!(params[:id])
-    # if current_user&.id == user.id
     @contents = current_user&.contents
-    # else
-    #   redirect_to root_path
-    # end
   end
 
 end
